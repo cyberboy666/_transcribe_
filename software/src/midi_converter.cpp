da0dbc15d0684aa6048329056c38af8eef171496
cyberboy666 <langolierz@gmail.com>
2022-04-27 14:52:56 +1200

    add booklet

--- software/src/midi_converter.cpp
+++ software/src/midi_converter.cpp
@@ -1,25 +1,25 @@
-// #include <usbmidi.h>
-// #include <SPI.h>
-// #include <AltSoftSerial.h>
+#include <usbmidi.h>
+#include <SPI.h>
+#include <AltSoftSerial.h>
 
-// AltSoftSerial altSerial;
+AltSoftSerial altSerial;
 
-// void setup() {
-// //   Serial1.begin(31250); //MIDI baudrate
-//   altSerial.begin(31250);
-// }
+void setup() {
+//   Serial1.begin(31250); //MIDI baudrate
+  altSerial.begin(31250);
+}
 
-// void loop() {
-//   //Handle USB communication
-//   USBMIDI.poll();
+void loop() {
+  //Handle USB communication
+  USBMIDI.poll();
 
-//   //Forward MIDI
-//   while(USBMIDI.available()){
-// altSerial.write(USBMIDI.read());
-// // Serial.write("receiving");
-//   }
+  //Forward MIDI
+  while(USBMIDI.available()){
+altSerial.write(USBMIDI.read());
+// Serial.write("receiving");
+  }
    
-//   altSerial.flush();
-//   while(altSerial.available()) Serial.write(altSerial.read());
-//   USBMIDI.flush();
-// }
+  altSerial.flush();
+  while(altSerial.available()) Serial.write(altSerial.read());
+  USBMIDI.flush();
+}
